1dbe20a3b723502ffe85e0cdaf45741aac2ffb19
Manu Chandrasekhar <manuchn@amazon.com>
2025-05-02 00:55:30 -0400

    checks only

--- examples/example_04_SNSTopicEncryption.tf
+++
@@ -1,8 +0,0 @@
-resource "awscc_sns_topic" "pass" {
-  topic_name = "pass"
-  kms_master_key_id = awscc_kms_key.example.arn
-}
-
-resource "awscc_sns_topic" "fail" {
-  topic_name = "fail"
-}
--- examples/example_27_GuarddutyDetectorEnabled.tf
+++
@@ -1,7 +0,0 @@
-resource "awscc_guardduty_detector" "pass" {
-  enable = true
-}
-
-resource "awscc_guardduty_detector" "fail" {
-  enable = false
-}
