45c87096047e84b6ea5e2056c436c48620918b84
Manu Chandrasekhar <manuchn@amazon.com>
2025-05-27 21:41:57 -0400

    fix: category setting for logging

--- custom_checks/awscc/checks/CloudWatchAlarmsEnabled.py
+++ custom_checks/awscc/checks/CloudWatchAlarmsEnabled.py
@@ -7,7 +7,7 @@ class CloudWatchAlarmsEnabled(BaseResourceCheck):
         name = "Ensure that CloudWatch alarm actions are enabled"
         id = "CKV_AWSCC_120"
         supported_resources = ["awscc_cloudwatch_alarm"]
-        categories = [CheckCategories.MONITORING]
+        categories = [CheckCategories. LOGGING]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
